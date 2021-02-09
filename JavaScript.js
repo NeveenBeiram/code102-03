85a7b43b8b0d92cb3cf165bc254ec2b089c0e6e7
NeveenBeiram <neveenbeiram@yahoo.com>
2021-02-09 15:55:38 +0200

    done the function

--- JavaScript.js
+++ JavaScript.js
@@ -1,17 +1,8 @@
-
- 
- 
- 
-var fname = prompt ('what is your name');
-//document.write(' Hello ${name}');
-//var age = alert('what is your age' );
-
+/*var fname = prompt ('what is your name');
 
 var con= confirm("confirm!");
 if (con == true) {
-    // console.log("test")
-    alert(`welcome ${fname}`);
-    // alert(`welcome ${fname}`);
+     alert(`welcome ${fname}`);
     document.write(`<h1>Hello ${fname}</h1>`);
     var age = newFunction();
 } else {
@@ -20,23 +11,19 @@ if (con == true) {
 }
 var isadult = true;
 if(age >=20){
-    //document.write(`${fname} ,is adult , ${isadult}`);
     console.log(`${fname} ,is adult , ${isadult}`)
 }
 else {
     isadult = false;
-    //document.write('${fname} , is adult , ${isadult}');
     console.log(`${fname} , is adult  ${isadult}`)
 }
+
 
 function newFunction() {
     return prompt(`${fname} what is your age`);
 }
 
-
-
 var order = prompt("what do you whant to order white flower or the red one (you must type 'red' or 'white' just)?")
-
 
 while(order != "white" && order != "red") {
     order = prompt("what do you whant to order white flower or the red one (you must type 'red' or 'white' just)?")
@@ -51,7 +38,7 @@ photo = `<img src="img/red.jpg">`
 if(order=="white"){
 photo = `<img width="1500px" hight="500" src="img/white2.jpg">`
 }
-//document.write ="test";
+
 var number = prompt("what is the number of flower bouquet you want ");
 var result = '';
 for(var i=0;i<number;i++){
@@ -59,4 +46,61 @@ result = result + photo ;
 
 }
 
-document.write(result);
+document.write(result);*/
+
+whatIsYourName();
+var fname = "";
+function whatIsYourName(){
+     fname = prompt ('what is your name');
+
+    var con= confirm("confirm!");
+if (con == true) {
+     alert(`welcome ${fname}`);
+    document.write(`<h1>Hello ${fname}</h1>`);
+    var age = newFunction();
+}else{
+    fname = prompt ('what is your name');
+    var age = newFunction();
+}
+var isadult = true;
+if(age >=20){
+    console.log(`${fname} ,is adult , ${isadult}`)
+}
+else {
+    isadult = false;
+    console.log(`${fname} , is adult  ${isadult}`)
+}
+theOrder();
+}
+
+function newFunction() {
+    return prompt(`${fname} what is your age`);
+}
+
+function theOrder(){
+    var order = prompt("what do you whant to order white flower or the red one (you must type 'red' or 'white' just)?")
+
+    while(order != "white" && order != "red") {
+        order = prompt("what do you whant to order white flower or the red one (you must type 'red' or 'white' just)?")
+    };
+    
+    var photo = "";
+    
+    if(order=="red"){
+    photo = `<img src="img/red.jpg">`
+    }
+    
+    if(order=="white"){
+    photo = `<img width="1500px" hight="500" src="img/white2.jpg">`
+    }
+    
+    var number = prompt("what is the number of flower bouquet you want ");
+    var result = '';
+    for(var i=0;i<number;i++){
+    result = result + photo ;
+    
+    }
+    
+    document.write(result);
+
+}
